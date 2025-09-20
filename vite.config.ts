72da8eced165a93dc48b15732a7f37246a1b4222
SalmaGira <Salma.Gira@lips.local>
2025-09-20 06:30:27 +0300

    fix: update JSX setting in tsconfig and replace Netlify plugin with React plugin in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
-import netlify from '@netlify/vite-plugin';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [netlify()],
+  plugins: [react()],
 })
